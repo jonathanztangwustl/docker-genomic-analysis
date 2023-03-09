7bf7f345f2038fd20efa8a6dc7ab29ed49ac6553
jonathanztangwustl <jonathanztang@wustl.edu>
2023-03-09 15:01:09 -0600

    Tweak to fix hg38 support in GenomeInfoDb

--- rpackages.R
+++ rpackages.R
@@ -1,3 +1,4 @@
-BiocManager::install(c("GenVisR","GenomicRanges","tximport","biomaRt","GenomeInfoDb","GenomicAlignments","Rsamtools","DESeq2","edgeR","DNAcopy"))
-install.packages(c("languageserver", "httpgd", "DNAcopy", "devtools"))
-install_github("broadinstitute/ichorCNA")
+BiocManager::install(c("GenVisR","GenomicRanges","tximport","biomaRt","GenomeInfoDb","GenomicAlignments","Rsamtools","DESeq2","edgeR","DNAcopy", "HMMcopy"))
+install.packages(c("languageserver", "httpgd", "DNAcopy", "devtools", "optparse"))
+#library("devtools")
+#install_github("broadinstitute/ichorCNA")
